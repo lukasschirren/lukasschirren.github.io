bf8c2d29fe22a0d9577ae81c780e3e926904d799
lukasschirren <l.schirren@web.de>
2025-03-31 18:51:21 +0000

    Deploying to gh-pages from @ lukasschirren/lukasschirren.github.io@589f84095dd9a081def9cfd8ef58202fc1740791 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-news",
+          title: "News",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
         },{id: "nav-publications",
           title: "Publications",
           description: "",
